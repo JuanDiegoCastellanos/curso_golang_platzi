c0d9a00b7e4d5244d1e692abdceb2bfd3da58e93
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2022-12-04 23:41:46 -0500

    for completed

--- src/main.go
+++ src/main.go
@@ -4,8 +4,24 @@ import "fmt"
 
 func main() {
 
+	// for condicional
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
+	}
+	fmt.Println("----------------")
+
+	// for while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter += 1
+	}
+
+	//for forever
+	counterForerever := 0
+	for {
+		fmt.Println(counterForerever)
+		counterForerever++
 	}
 
 }
