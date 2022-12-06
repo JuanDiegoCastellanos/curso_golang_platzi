bb6b1b0c6c73428035d9abe631c1e9785bb75907
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2022-12-05 23:41:33 -0500

    if and condicionales

--- src/main.go
+++ src/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 func main() {
 
@@ -23,5 +27,34 @@ func main() {
 		fmt.Println(counterForerever)
 		counterForerever++
 	}
+
+	// condicionales
+	valor1 := 1
+	valor2 := 2
+
+	if valor1 == 1 {
+		fmt.Println("Es 1")
+	} else {
+		fmt.Println("No es 1")
+	}
+
+	// With and
+	if valor1 == 1 && valor2 == 2 {
+		fmt.Println("Es verdad")
+	}
+
+	// With or
+	if valor1 == 0 || valor2 == 2 {
+		fmt.Println("Es verdad, OR")
+	}
+
+	// Convertir texto a numero
+	value, err := strconv.Atoi("53")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Value: ", value)
 
 }
