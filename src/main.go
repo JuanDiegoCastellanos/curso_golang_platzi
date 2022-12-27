f7ec4c94742567935835979bad8de27767449539
Juan Diego Castellanos Jerez <juancastellanosj15@gmail.com>
2022-12-27 18:17:47 -0500

    concurrency with goroutines

--- src/main.go
+++ src/main.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-func say(text string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println(text)
+//	func say(text string, wg *sync.WaitGroup) {
+//		defer wg.Done()
+//		fmt.Println(text)
+//	}
+func sayTwo(msj string, c chan<- string) {
+	c <- msj
 }
 
 func main() {
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 
 	fmt.Println("Concurrencia trabaja con multiples cosas al tiempo")
 	fmt.Println("Gorutines")
 
-	say("Hello", &wg)
-	wg.Add(1)
-	go say("World", &wg)
+	// say("Hello", &wg)
+	// wg.Add(1)
+	// go say("World", &wg)
 
-	wg.Wait()
+	// wg.Wait()
 
 	go func(text string) {
 		fmt.Println(text)
 	}("Adios")
 
 	// time.Sleep(time.Second * 1)
+	c := make(chan string, 1)
+	fmt.Println("Hello")
+
+	go sayTwo("Bye", c)
+
+	fmt.Println(<-c)
 }
